a37d64105ba6c78b7b9075dd3ad07ffde0cc2fa0
Cameron Otsuka <cameron@otsuka.haus>
2024-08-05 12:09:22 -0700

    add opengraph meta

--- eleventy.config.js
+++ eleventy.config.js
@@ -29,9 +29,9 @@ module.exports = function (eleventyConfig) {
 		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
 		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
 	});
-	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
+	eleventyConfig.addFilter("isoDate", (dateObj) => {
 		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
-		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "dd LLLL yyyy");
+		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toISO();
 	});
 	eleventyConfig.addPassthroughCopy({ "./static/": "/" });
 	eleventyConfig.addPlugin(feedPlugin, {
