4f9b87b255e2f0625745088ffd494c58f814f9dd
Cameron Otsuka <cameron@otsuka.haus>
2025-06-15 13:48:46 -0700

    remove json import

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
-import Metadata from 'data/metadata.json';
 
 export default defineConfig({
   site: 'https://otsuka.haus',
