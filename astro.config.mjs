6f79dfd3798901886fd1d4d1143c0e5364e851b4
Cameron Otsuka <cameron@otsuka.haus>
2025-06-15 13:46:56 -0700

    import json directly

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
-import Metadata from '@data/metadata.json';
+import Metadata from 'data/metadata.json';
 
 export default defineConfig({
   site: Metadata.base,
