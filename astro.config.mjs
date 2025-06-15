53f48e931cb5676cb7dafa1d21607aa4ab00f841
Cameron Otsuka <cameron@otsuka.haus>
2025-06-15 14:15:53 -0700

    add rss feed, robots, and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://otsuka.haus',
   trailingSlash: 'always',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
---
+++ src/pages/feed.xml.ts
@@ -0,0 +1,36 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { formatDate } from '@utils/format.ts';
+import Metadata from '@data/metadata.json';
+
+export async function GET(context) {
+  const articles = await getCollection('articles');
+  const articleItems = articles.map((article) => ({
+    title: article.data.title,
+    pubDate: article.data.date,
+    description: article.data.description,
+    link: `/articles/${formatDate(article.data.date)}-${article.id}/`
+  }));
+  const links = await getCollection('links');
+  const linkItems = links.map((link) => ({
+    title: link.data.title,
+    pubDate: link.data.date,
+    description: link.data.description,
+    link: `/links/${formatDate(link.data.date)}-${link.id}/`
+  }));
+  const reviews = await getCollection('reviews');
+  const reviewItems = reviews.map((review) => ({
+    title: review.data.title,
+    pubDate: review.data.date,
+    description: review.data.description,
+    link: `/reviews/${formatDate(review.data.date)}-${review.id}/`
+  }));
+  const feedItems = [ ...articleItems, ...linkItems, ...reviewItems ];
+  const sortedFeedItems = feedItems.sort((a, b) => b.pubDate - a.pubDate);
+  return rss({
+    title: Metadata.title,
+    description: Metadata.subtitle,
+    site: context.site,
+    items: sortedFeedItems
+  });
+}
