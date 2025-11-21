381c578bde7e21e3b7d1d10e1b1ce2f47c2a2cb8
Cameron Otsuka <cameron@otsuka.haus>
2025-11-20 17:22:28 -0800

    add categories to rss feed

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -7,25 +7,28 @@ export async function GET(context: any) {
   const articles = await getCollection('articles');
   const articleItems = articles.map((article) => ({
     title: article.data.title,
+    link: `/articles/${formatDate(article.data.date)}-${article.id}/`,
     pubDate: article.data.date,
     description: article.data.description,
-    link: `/articles/${formatDate(article.data.date)}-${article.id}/`
+    categories: (article.data.tags ?? []).concat('articles')
   }));
 
   const roundups = await getCollection('roundups');
   const roundupItems = roundups.map((roundup) => ({
     title: roundup.data.title,
+    link: `/build-weekly-roundup/${roundup.id}/`,
     pubDate: roundup.data.date,
     description: roundup.data.description,
-    link: `/build-weekly-roundup/${roundup.id}/`
+    categories: (roundup.data.tags ?? []).concat('roundups')
   }));
 
   const reviews = await getCollection('reviews');
   const reviewItems = reviews.map((review) => ({
     title: review.data.title,
+    link: `/reviews/${review.data.type}/${review.id}/`,
     pubDate: review.data.date,
     description: review.data.description,
-    link: `/reviews/${review.data.type}/${review.id}/`
+    categories: (review.data.tags ?? []).concat(review.data.type).concat('reviews')
   }));
 
   const feedItems = [ ...articleItems, ...roundupItems, ...reviewItems ];
