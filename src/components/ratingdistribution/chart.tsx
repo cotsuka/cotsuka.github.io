829e69559e0e1bcd13438ecc8e23349475aedc5c
Cameron Otsuka <cameron@otsuka.haus>
2025-12-10 17:28:57 -0800

    finalize color scheme

--- src/components/ratingdistribution/chart.tsx
+++ src/components/ratingdistribution/chart.tsx
@@ -1,19 +1,23 @@
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, ResponsiveContainer, Tooltip } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer, Tooltip } from 'recharts';
 
 export default function Chart({ chartData }: { chartData: { name: string, count: number }[] }) {
     return (
         <ResponsiveContainer width="100%" height={150}>
             <BarChart data={chartData}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis width="auto"/>
-                <Tooltip          
-                    contentStyle={{ color: "light-dark(#000000, #000000)" }}
-                    itemStyle={{ color: "light-dark(#000000, #000000)" }}
-                    labelStyle={{ color: "light-dark(#000000, #000000)" }}
-                    wrapperStyle={{ color: "light-dark(#000000, #000000)" }}
+                <XAxis
+                    dataKey="name"
+                    tick={{ fill: "light-dark(#000000, #e6e2d6)" }}
                 />
-                <Bar dataKey="count" fill="light-dark(#000000, #ffffff)" />
+                <YAxis
+                    width="auto"
+                    tick={{ fill: "light-dark(#000000, #e6e2d6)" }}
+                />
+                <Tooltip          
+                    contentStyle={{ backgroundColor: "light-dark(#000000, #e6e2d6)" }}
+                    itemStyle={{ color: "light-dark(#e6e2d6, #000000)" }}
+                    labelStyle={{ color: "light-dark(#e6e2d6, #000000)" }}
+                />
+                <Bar dataKey="count" fill="light-dark(#000000, #e6e2d6)" />
             </BarChart>
         </ResponsiveContainer>
     )
