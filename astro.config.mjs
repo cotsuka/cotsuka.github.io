5bca1b99550538644e38d9e4bf364b45298da960
Cameron Otsuka <cameron@otsuka.haus>
2025-12-29 23:12:45 -0800

    cache path does need to be separate

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://cameron.otsuka.systems',
+  cacheDir: './.astro-cache',
   integrations: [
     icon({
       include: {
